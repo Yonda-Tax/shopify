3b268e1d758ecedb002291bf47751195d8b58328
Sean O'Reilly <sean.oreilly@yondatax.com>
2022-11-12 12:18:46 +0200

    Update schema file

--- web/db/schema.rb
+++ web/db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_09_125631) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_12_100241) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "gdpr_requests", force: :cascade do |t|
+    t.string "request_type"
+    t.text "request_json"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "shops", force: :cascade do |t|
     t.string "shopify_domain", null: false
